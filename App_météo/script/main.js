706104d330eca32a3a5373ef6d4f368c2703f4ca
Alexandre <killersgg78@gmail.com>
2022-02-16 12:40:43 +0100

    workign previsions

--- App_météo/script/main.js
+++ App_météo/script/main.js
@@ -3,6 +3,13 @@
 const APIkey = "0066293670bca9338e82e9b29fee2be2"
 let resultsAPI;
 
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const location = document.querySelector('.localisation');
+const hours = document.querySelectorAll('.heure-nom-prevision')
+const tempforH = document.querySelectorAll('.heure-prevision-valeur')
+
+
 if (navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position => {
 
@@ -21,7 +28,24 @@ function AppelAPI(lon, lat) {
         return answer.json();
     })
     .then((data) =>{
-        console.log(data);
+        
+        resultsAPI = data;
+        temps.innerText = resultsAPI.current.weather[0].description;
+        temperature.innerText = `${Math.round(resultsAPI.current.temp)}°C`;
+        location.innerText = resultsAPI.timezone;
+
+        // weather every 3 hours
+
+        let timenow = new Date().getHours();
+
+        for (let i = 0; i< hours.length; i++){
+
+            let increHours = (timenow + i * 3)%24;
+            hours[i].innerText = `${increHours} h`;
+        }
+        for (let j = 0; j < tempforH.length; j++) {
+            tempforH[j].innerText = `${Math.round(resultsAPI.hourly[j * 3].temp)}°C`
+        }
     })
 }
 
